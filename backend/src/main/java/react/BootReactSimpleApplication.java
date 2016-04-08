b0ea6f3d86aabc6a7a3d1a4fd9d168328d84ccba
geowarin <geowarin@gmail.com>
2016-04-08 23:08:52 +0200

    Add CORS for development

--- backend/src/main/java/react/BootReactSimpleApplication.java
+++ backend/src/main/java/react/BootReactSimpleApplication.java
@@ -2,11 +2,27 @@ package react;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BootReactSimpleApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BootReactSimpleApplication.class, args);
+  }
+
+  @Bean
+  @Profile("!production")
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**/*").allowedOrigins("http://localhost:3000");
+      }
+    };
   }
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 export default class App extends React.Component {
+
+  componentDidMount () {
+    fetch('http://localhost:8080/api/hello').then(r => console.log(r.body));
+  }
+
   render () {
     return <div>
       <h2>Welcome to React</h2>
