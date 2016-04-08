95f896119fe6fb6eb67963aba4123a7a905e636c
geowarin <geowarin@gmail.com>
2016-04-08 23:33:03 +0200

    Add some css

--- frontend/nwb.config.js
+++ frontend/nwb.config.js
@@ -1,7 +1,19 @@
+var path = require('path');
+
 module.exports = {
   type: 'react-app',
   babel: {
     stage: 0,
     optional: ['runtime']
+  },
+  webpack: {
+    extra: {
+      resolve: {
+        alias: {
+          'components': path.resolve(__dirname, 'src/components'),
+          'css': path.resolve(__dirname, 'assets/css')
+        }
+      }
+    }
   }
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
-import React from "react";
-import {render} from "react-dom";
-import App from "./App";
+import React from 'react';
+import {render} from 'react-dom';
+import App from 'components/App';
+
+import 'css/main.css';
 
 render(<App/>, document.querySelector('#app'));
