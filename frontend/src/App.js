ca4cab79f8060b739f9287b82e613e08869c89cb
geowarin <geowarin@gmail.com>
2016-04-08 18:35:06 +0200

    Upgrade to react 15

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
-export default React.createClass({
-  render() {
+export default class App extends React.Component {
+  render () {
     return <div>
       <h2>Welcome to React</h2>
     </div>
   }
-})
+}
--- frontend/tests/App-test.js
+++ frontend/tests/App-test.js
@@ -1,23 +1,24 @@
-import expect from 'expect'
-import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import expect from 'expect';
+import React from 'react';
+import {render, unmountComponentAtNode} from 'react-dom';
 
-import App from 'src/App'
+import App from 'src/App';
 
 describe('App component', () => {
-  let node
+  let node;
 
   beforeEach(() => {
     node = document.createElement('div')
-  })
+  });
 
   afterEach(() => {
     unmountComponentAtNode(node)
-  })
+  });
 
   it('displays a welcome message', () => {
     render(<App/>, node, () => {
       expect(node.innerHTML).toContain('Welcome to React')
     })
-  })
-})
+  });
+
+});
